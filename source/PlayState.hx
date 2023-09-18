f521ac75e8bb8dd885e5211e4aba5ac3081b2f6b
RevixVRO <142174270+ZenonDeveloper@users.noreply.github.com>
2023-09-18 22:39:22 +0100

    Revamped Entire Game

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxSpriteGroup;
 import flixel.input.keyboard.FlxKey;
 import flixel.math.FlxRandom;
 import flixel.util.FlxColor;
@@ -14,191 +15,181 @@ class PlayState extends FlxState
 	var Note2:FlxSprite;
 	var Note3:FlxSprite;
 
-	var Notes = [];
+	var NoteGroup:FlxSpriteGroup;
 
-	var score = 0;
+	var noteProb = 15;
+	var yAxisGlobal = 800;
 
-	public override function create()
+	override public function create()
 	{
 		Note = new FlxSprite();
 		Note.makeGraphic(50, 50, FlxColor.WHITE);
 		Note.screenCenter(X);
-		Note.y = 700;
-		Note.x -= 400;
+		Note.y = yAxisGlobal;
+		Note.x -= 200;
 		add(Note);
 
 		Note1 = new FlxSprite();
 		Note1.makeGraphic(50, 50, FlxColor.WHITE);
 		Note1.screenCenter(X);
-		Note1.y = 700;
-		Note1.x -= 300;
+		Note1.y = yAxisGlobal;
+		Note1.x -= 100;
 		add(Note1);
 
 		Note2 = new FlxSprite();
 		Note2.makeGraphic(50, 50, FlxColor.WHITE);
 		Note2.screenCenter(X);
-		Note2.y = 700;
-		Note2.x -= 200;
+		Note2.y = yAxisGlobal;
+		Note2.x += 100;
 		add(Note2);
 
 		Note3 = new FlxSprite();
 		Note3.makeGraphic(50, 50, FlxColor.WHITE);
 		Note3.screenCenter(X);
-		Note3.y = 700;
-		Note3.x -= 100;
+		Note3.y = yAxisGlobal;
+		Note3.x += 200;
 		add(Note3);
+
+		NoteGroup = new FlxSpriteGroup();
+
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
-
-		keyPresser();
-		noteSpawner();
+		manageKeybinds();
+		endlessNotes();
 	}
 
-	public function HitTheNote(Obj1:FlxSprite, Obj2:FlxSprite)
+	public function noteOneCollide(i:FlxSprite, strumNote:FlxSprite)
 	{
-		Sys.sleep(0.005);
-
-		switch Obj1.ID
+		if (i.ID == 0)
 		{
-			case 401:
-				{
-					if (FlxG.keys.anyPressed([FlxKey.D]))
-					{
-						Obj1.destroy();
-						score++;
-					}
-				}
-			case 402:
-				{
-					if (FlxG.keys.anyPressed([FlxKey.F]))
-					{
-						Obj1.destroy();
-						score++;
-					}
-				}
-			case 403:
-				{
-					if (FlxG.keys.anyPressed([FlxKey.J]))
-					{
-						Obj1.destroy();
-						score++;
-					}
-				}
-			case 404:
-				{
-					if (FlxG.keys.anyPressed([FlxKey.K]))
-					{
-						Obj1.destroy();
-						score++;
-					}
-				}
+			if (FlxG.keys.anyPressed([FlxKey.D]))
+			{
+				i.destroy();
+			}
 		}
-	}
 
-	public function noteSpawner()
-	{
-		if (FlxG.random.float(0.1, 1.0) >= 0.9)
+		if (i.ID == 1)
 		{
-			var randInt = FlxG.random.int(1, 4);
-
-			if (randInt == 1)
+			if (FlxG.keys.anyPressed([FlxKey.F]))
 			{
-				var GayNote = new FlxSprite();
-				GayNote.makeGraphic(40, 40, FlxColor.WHITE);
-				GayNote.screenCenter(X);
-				GayNote.y = 0;
-				GayNote.x -= 100;
-				add(GayNote);
-
-				GayNote.ID = 401;
-				FlxG.collide(GayNote, Note, HitTheNote);
-
-				Notes.push(GayNote);
+				i.destroy();
 			}
-			else if (randInt == 2)
+		}
+
+		if (i.ID == 2)
+		{
+			if (FlxG.keys.anyPressed([FlxKey.J]))
 			{
-				var GayNote = new FlxSprite();
-				GayNote.makeGraphic(20, 20, FlxColor.WHITE);
-				GayNote.screenCenter(X);
-				GayNote.y = 0;
-				GayNote.x -= 200;
-				add(GayNote);
-
-				GayNote.ID = 402;
-				FlxG.collide(GayNote, Note, HitTheNote);
-
-				Notes.push(GayNote);
+				i.destroy();
 			}
-			else if (randInt == 3)
+		}
+
+		if (i.ID == 3)
+		{
+			if (FlxG.keys.anyPressed([FlxKey.K]))
 			{
-				var GayNote = new FlxSprite();
-				GayNote.makeGraphic(20, 20, FlxColor.WHITE);
-				GayNote.screenCenter(X);
-				GayNote.y = 0;
-				GayNote.x -= 300;
-				add(GayNote);
-
-				GayNote.ID = 403;
-				FlxG.collide(GayNote, Note, HitTheNote);
-
-				Notes.push(GayNote);
-			}
-			else if (randInt == 4)
-			{
-				var GayNote = new FlxSprite();
-				GayNote.makeGraphic(20, 20, FlxColor.WHITE);
-				GayNote.screenCenter(X);
-				GayNote.y = 0;
-				GayNote.x -= 400;
-				add(GayNote);
-
-				GayNote.ID = 404;
-				FlxG.collide(GayNote, Note, HitTheNote);
-
-				Notes.push(GayNote);
-			}
-
-			for (k in Notes)
-			{
-				k.y += 5;
-
-				if (k.y == 700)
-					HitTheNote(k, Note);
+				i.destroy();
 			}
 		}
 	}
 
-	public function keyPresser()
+	public function endlessNotes()
+	{
+		for (i in NoteGroup)
+		{
+			i.y += 10;
+			FlxG.overlap(i, Note, noteOneCollide);
+			FlxG.overlap(i, Note1, noteOneCollide);
+			FlxG.overlap(i, Note2, noteOneCollide);
+			FlxG.overlap(i, Note3, noteOneCollide);
+			if (i.y >= 900)
+			{
+				i.destroy();
+			}
+		}
+
+		var randomGeneration = FlxG.random.int(0, noteProb);
+		if (randomGeneration == 1)
+		{
+			var randomInt = FlxG.random.int(0, 3);
+			trace(randomInt);
+			if (randomInt == 0)
+			{
+				var RandomNote = new FlxSprite();
+				RandomNote.makeGraphic(50, 50, FlxColor.WHITE);
+				RandomNote.screenCenter(X);
+				RandomNote.ID = 0;
+				RandomNote.x -= 200;
+				add(RandomNote);
+				NoteGroup.add(RandomNote);
+			}
+
+			if (randomInt == 1)
+			{
+				var RandomNote = new FlxSprite();
+				RandomNote.makeGraphic(50, 50, FlxColor.WHITE);
+				RandomNote.screenCenter(X);
+				RandomNote.ID = 1;
+				RandomNote.x -= 100;
+				add(RandomNote);
+				NoteGroup.add(RandomNote);
+			}
+
+			if (randomInt == 2)
+			{
+				var RandomNote = new FlxSprite();
+				RandomNote.makeGraphic(50, 50, FlxColor.WHITE);
+				RandomNote.screenCenter(X);
+				RandomNote.ID = 2;
+				RandomNote.x += 100;
+				add(RandomNote);
+				NoteGroup.add(RandomNote);
+			}
+
+			if (randomInt == 3)
+			{
+				var RandomNote = new FlxSprite();
+				RandomNote.makeGraphic(50, 50, FlxColor.WHITE);
+				RandomNote.screenCenter(X);
+				RandomNote.ID = 3;
+				RandomNote.x += 200;
+				add(RandomNote);
+				NoteGroup.add(RandomNote);
+			}
+		}
+	}
+
+	public function manageKeybinds()
 	{
 		if (FlxG.keys.anyPressed([FlxKey.D]))
 		{
-			Note.color = FlxColor.PINK;
+			Note.color = FlxColor.RED;
 		}
 		else
 		{
-			Note.color = FlxColor.GRAY;
+			Note.color = FlxColor.WHITE;
 		}
 
 		if (FlxG.keys.anyPressed([FlxKey.F]))
 		{
-			Note1.color = FlxColor.BLUE;
+			Note1.color = FlxColor.RED;
 		}
 		else
 		{
-			Note1.color = FlxColor.GRAY;
+			Note1.color = FlxColor.WHITE;
 		}
 
 		if (FlxG.keys.anyPressed([FlxKey.J]))
 		{
-			Note2.color = FlxColor.GREEN;
+			Note2.color = FlxColor.RED;
 		}
 		else
 		{
-			Note2.color = FlxColor.GRAY;
+			Note2.color = FlxColor.WHITE;
 		}
 
 		if (FlxG.keys.anyPressed([FlxKey.K]))
@@ -207,7 +198,7 @@ class PlayState extends FlxState
 		}
 		else
 		{
-			Note3.color = FlxColor.GRAY;
+			Note3.color = FlxColor.WHITE;
 		}
 	}
 }
